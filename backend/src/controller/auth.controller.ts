692a736d2d652ba140e936823a3634e625e5c949
SarthakGupta345 <Sarthak987gupta654@gmail.com>
2026-06-21 15:18:02 +0530

    answer models tarted

---
+++ backend/src/controller/auth.controller.ts
@@ -0,0 +1,8 @@
+export const signup = async (req : Request , res : Response) => {
+    try {
+        const {name,email,password} = req.body;
+        
+    } catch (error) {
+        
+    }
+}
---
+++ backend/src/middleware/auth.middleware.ts
@@ -0,0 +1,49 @@
+import { NextFunction, Response, Request } from "express";
+import jwt from "jsonwebtoken";
+
+interface User {
+    _id: string;
+    email: string;
+    name: string;
+}
+
+interface AuthenticatedRequest extends Request {
+    user: User;
+}
+
+export const loginMiddleware = (
+    req: AuthenticatedRequest,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+        const token =
+            req.cookies.token ||
+            (authHeader?.startsWith("Bearer ")
+                ? authHeader.split(" ")[1]
+                : authHeader);
+
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized",
+            });
+        }
+
+        const decodedToken = jwt.verify(
+            token,
+            process.env.JWT_SECRET!
+        );
+
+        req.user = decodedToken as User;
+
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: "Invalid Token",
+        });
+    }
+};
