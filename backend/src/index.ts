56f2a248173ad88d3149fb7d5654e97f85c0aa96
SarthakGupta345 <Sarthak987gupta654@gmail.com>
2026-06-21 13:16:08 +0530

    Project started

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser())
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+app.use(express.urlencoded({
+    extended: true,
+    limit: "50kb"
+}))
+
+app.get("/", (req, res) => {
+    res.send("Hello TypeScript");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
